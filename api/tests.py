8bbe1701762fcce2a9d080612db71cedecbbc450
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2019-05-29 14:59:00 +0100

    added another test

--- api/tests.py
+++ api/tests.py
@@ -5,3 +5,7 @@ class EventTest(TestCase):
     def test_str(self):
         event = Event.objects.create(name='MYname', is_vip=True)
         self.assertEqual(str(event), "{}:{}".format(event.name, event.is_vip))
+
+    def test_str_2(self):
+        event = Event.objects.create(name='MYname', is_vip=True)
+        self.assertEqual(str(event), "{}:{}".format(event.name, event.is_vip))
